a4e692549b38650584a0bb32dd1a7bfa2189232e
Aditya Pradeep <126142943+aditya-pradeep3011@users.noreply.github.com>
2026-03-31 18:03:33 +0530

    Added api gateway and security
    Added an api gateway with OAuth2 security using keycloak

---
+++ api-gateway/src/main/java/com/libms/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.libms.api_gateway.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+				.cors(cors -> cors.configurationSource(getCorsConfigurationSource()))
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+				.build();
+	}
+	
+	@Bean
+	CorsConfigurationSource getCorsConfigurationSource()
+	{
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.applyPermitDefaultValues();
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH", "OPTIONS", "HEAD"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
+	}
+}
---
+++ api-gateway/src/main/java/com/libms/api_gateway/route/Routes.java
@@ -0,0 +1,55 @@
+package com.libms.api_gateway.route;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+	
+	@Value("${product.service.url}")
+	private String productServiceUrl;
+	
+	@Value("${inventory.service.url}")
+	private String inventoryServiceUrl;
+	
+	@Value("${order.service.url}")
+	private String orderServiceUrl;
+
+	@Bean
+	RouterFunction<ServerResponse> productServiceRoute()
+	{
+		return GatewayRouterFunctions.route("product-service")
+				.route(RequestPredicates.path("/api/books"), HandlerFunctions.http())
+				.before(BeforeFilterFunctions.uri(productServiceUrl))
+//				.filter(TokenRelayFilterFunctions.tokenRelay())
+				.build();
+	}
+	
+	@Bean
+	RouterFunction<ServerResponse> inventoryServiceRoute()
+	{
+		return GatewayRouterFunctions.route("inventory-service")
+				.route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http())
+				.before(BeforeFilterFunctions.uri(inventoryServiceUrl))
+//				.filter(TokenRelayFilterFunctions.tokenRelay())
+				.build();
+	}
+	
+	@Bean
+	RouterFunction<ServerResponse> orderServiceRoute()
+	{
+		return GatewayRouterFunctions.route("order-service")
+				.route(RequestPredicates.path("/api/orders"), HandlerFunctions.http())
+				.before(BeforeFilterFunctions.uri(orderServiceUrl))
+//				.filter(TokenRelayFilterFunctions.tokenRelay())
+				.build();
+	}
+}
